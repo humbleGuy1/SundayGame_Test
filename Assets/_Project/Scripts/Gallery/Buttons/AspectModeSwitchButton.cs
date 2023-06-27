4812f5c5e4a319b26bee8af5c6e4a29dac6be55b
humbleGuy1 <vrudnevskiy@list.ru>
2023-06-27 21:12:13 +0400

    Add aspect mode button

---
+++ Assets/_Project/Scripts/Gallery/Buttons/AspectModeSwitchButton.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SunGameStudio.Gallery
+{
+    public class AspectModeSwitchButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private AspectRatioFitter _aspectRatioFitter;
+
+        private void OnEnable() => _button.onClick.AddListener(Switch);
+
+        private void OnDisable() => _button.onClick.RemoveListener(Switch);
+
+        private void Switch()
+        {
+            if (_aspectRatioFitter.aspectMode == AspectRatioFitter.AspectMode.EnvelopeParent)
+                EnableFitMode();
+            else
+                EnableEnvelopeMode();
+        }
+
+        private void EnableFitMode()
+        {
+            _aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+            _text.text = "Fit";
+        }
+
+        private void EnableEnvelopeMode()
+        {
+            _aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.EnvelopeParent;
+            _text.text = "Envelope";
+        }
+    }
+}
+
+
